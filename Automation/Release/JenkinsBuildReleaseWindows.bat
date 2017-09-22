50f565023b9e6bba0372a6de29c5a5426b5e042c
Terence Darwen <tmdarwen@gmail.com>
2017-09-22 16:14:18 -0500

    Adding release build script for Windows

---
+++ Automation/Release/JenkinsBuildReleaseWindows.bat
@@ -0,0 +1,31 @@
+ECHO Building PhaseVocoder version number %Version% 
+ECHO Build number is %BUILD_NUMBER%
+
+REM Switch to the tag
+git checkout -b PhaseVocoderTag v%Version%
+
+REM Run CMake
+mkdir Build
+cd Build
+cmake -D VERSION_NUMBER=%Version% -D BUILD_NUMBER=%BUILD_NUMBER% -G "%CMakeGeneratorString%" ..\Source
+IF %ERRORLEVEL% NEQ 0 (
+    ECHO CMake failed
+    EXIT 1
+)
+
+REM Build only PhaseVocoder
+cd PhaseVocoder
+devenv PhaseVocoder.vcxproj /Build Release
+IF %ERRORLEVEL% NEQ 0 (
+    ECHO The build failed    
+    EXIT 1
+)
+
+REM Zip the executable
+echo "7z" a -tzip PhaseVocoder.zip "%WORKSPACE%\Build\Application\Release\PhaseVocoder.exe" 
+"7z" a -tzip PhaseVocoder.zip "%WORKSPACE%\Build\Application\Release\PhaseVocoder.exe" 
+
+IF %ERRORLEVEL% NEQ 0 (
+    ECHO Zipping PhaseVocoder.exe failed
+    EXIT 1
+)
