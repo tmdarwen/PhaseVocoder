a63bb7f71bd1d82e5acd09566886d497ae7a6063
Terence Darwen <tmdarwen@gmail.com>
2017-09-22 15:25:46 -0500

    Fixed path for cmake OSX script

--- Automation/CI/JenkinsBuildCIOSX.sh
+++ Automation/CI/JenkinsBuildCIOSX.sh
@@ -1,7 +1,7 @@
 # Do a debug build
 mkdir DebugBuild
 cd DebugBuild
-/usr/local/bin/cmake -G "Xcode" -DCMAKE_BUILD_TYPE=Debug ../
+/usr/local/bin/cmake -G "Xcode" -DCMAKE_BUILD_TYPE=Debug ../Source
 xcodebuild -project PhaseVocoder.xcodeproj -configuration Debug
 
 cd ..
@@ -9,5 +9,5 @@ cd ..
 # Do a releaase build
 mkdir ReleaseBuild
 cd ReleaseBuild
-/usr/local/bin/cmake -G "Xcode" -DCMAKE_BUILD_TYPE=Release ../
+/usr/local/bin/cmake -G "Xcode" -DCMAKE_BUILD_TYPE=Release ../Source
 xcodebuild -project PhaseVocoder.xcodeproj -configuration Release
