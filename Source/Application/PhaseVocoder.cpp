3a14152136c519014c027fc102cd8477e88df283
Terence Darwen <tmdarwen@gmail.com>
2017-10-23 12:16:56 -0500

    Added formatting to display of transients

--- Source/Application/PhaseVocoder.cpp
+++ Source/Application/PhaseVocoder.cpp
@@ -27,6 +27,9 @@
 #include <iostream>
 #include <string>
 #include <algorithm>
+#include <iomanip>
+#include <locale>
+#include <sstream>
 #include <Utilities/Exception.h>
 #include <Application/PhaseVocoderMediator.h>
 #include <Application/CommandLineArguments.h>
@@ -34,6 +37,20 @@
 
 const uint32_t SUCCESS{0};
 const uint32_t FAILURE{1};
+
+void CheckCommandLineArguments(CommandLineArguments& commandLineArguments);
+std::unique_ptr<PhaseVocoderMediator> GetPhaseVocoderMediator(const CommandLineArguments& commandLineArguments);
+int PerformPhaseVocoding(CommandLineArguments& commandLineArguments);
+void DisplayTransients(const std::unique_ptr<PhaseVocoderMediator>& phaseVocoderMediator);
+void DisplayAllTransientsOnChannel(const std::vector<std::size_t>& transients);
+
+int main(int argc, char* argv[])
+{
+	CommandLineArguments commandLineArguments(argc, argv);
+
+	CheckCommandLineArguments(commandLineArguments);
+	return PerformPhaseVocoding(commandLineArguments);
+}
 
 void CheckCommandLineArguments(CommandLineArguments& commandLineArguments)
 {
@@ -103,50 +120,12 @@ std::unique_ptr<PhaseVocoderMediator> GetPhaseVocoderMediator(const CommandLineA
 		phaseVocoderSettings.SetValleyToPeakRatio(commandLineArguments.GetValleyPeakRatio());
 	}
 
+	if(commandLineArguments.ShowTransients())
+	{
+		phaseVocoderSettings.SetDisplayTransients();
+	}
+
 	return std::unique_ptr<PhaseVocoderMediator>{new PhaseVocoderMediator(phaseVocoderSettings)};
-}
-
-void DisplayTransients(const std::unique_ptr<PhaseVocoderMediator>& phaseVocoderMediator)
-{
-	auto printSamplePosition{[](const std::size_t& samplePosition) { std::cout << " " << samplePosition; }};
-
-	if(phaseVocoderMediator->GetChannelCount() == 1)
-	{
-		auto transients{phaseVocoderMediator->GetTransients(0)};
-
-		if(transients.size() == 0)
-		{
-			std::cout << "No transients found" << std::endl;	
-		}
-		else
-		{
-			std::cout << "Transient sample positions:";
-			std::for_each(transients.begin(), transients.end(), printSamplePosition);
-		}
-	}
-	else if(phaseVocoderMediator->GetChannelCount() == 2)
-	{
-		auto leftTransients{phaseVocoderMediator->GetTransients(0)};
-		auto rightTransients{phaseVocoderMediator->GetTransients(1)};
-
-		if(leftTransients.size() == 0 && rightTransients.size() == 0)
-		{
-			std::cout << "No transients found" << std::endl;	
-		}
-
-		if(leftTransients.size())
-		{
-			std::cout << "Left channel transient sample positions:";
-			std::for_each(leftTransients.begin(), leftTransients.end(), printSamplePosition);
-			std::cout << std::endl;
-		}
-
-		if(rightTransients.size())
-		{
-			std::cout << "Right channel transient sample positions:";
-			std::for_each(rightTransients.begin(), rightTransients.end(), printSamplePosition);
-		}
-	}
 }
 
 int PerformPhaseVocoding(CommandLineArguments& commandLineArguments)
@@ -178,10 +157,87 @@ int PerformPhaseVocoding(CommandLineArguments& commandLineArguments)
 	return SUCCESS;
 }
 
-int main(int argc, char* argv[])
+void DisplayTransients(const std::unique_ptr<PhaseVocoderMediator>& phaseVocoderMediator)
 {
-	CommandLineArguments commandLineArguments(argc, argv);
 
-	CheckCommandLineArguments(commandLineArguments);
-	return PerformPhaseVocoding(commandLineArguments);
+	if(phaseVocoderMediator->GetChannelCount() == 1)
+	{
+		auto transients{phaseVocoderMediator->GetTransients(0)};
+
+		if(transients.size() == 0)
+		{
+			std::cout << "No transients found" << std::endl;	
+		}
+		else
+		{
+			std::cout << "Transient sample positions:";
+			DisplayAllTransientsOnChannel(transients);
+		}
+	}
+	else if(phaseVocoderMediator->GetChannelCount() == 2)
+	{
+		auto leftTransients{phaseVocoderMediator->GetTransients(0)};
+		auto rightTransients{phaseVocoderMediator->GetTransients(1)};
+
+		if(leftTransients.size() == 0 && rightTransients.size() == 0)
+		{
+			std::cout << "No transients found" << std::endl;	
+		}
+
+		if(leftTransients.size())
+		{
+			std::cout << "Left channel transient sample positions:";
+			DisplayAllTransientsOnChannel(leftTransients);
+			std::cout << std::endl;
+		}
+
+		if(rightTransients.size())
+		{
+			std::cout << "Right channel transient sample positions:";
+			DisplayAllTransientsOnChannel(rightTransients);
+		}
+	}
+}
+
+void DisplayAllTransientsOnChannel(const std::vector<std::size_t>& transients)
+{
+	std::size_t counter{0};
+	const std::size_t minimumTableSize{8};
+
+	auto ConvertNumberToStringWithCommas{[](std::size_t number)
+	{
+		std::stringstream ss;
+		ss.imbue(std::locale(""));
+		ss << std::fixed << number;
+		return ss.str();
+	}};
+
+	auto PrintSamplePositionInLine{[&ConvertNumberToStringWithCommas](const std::size_t& samplePosition) 
+	{
+		std::cout << " " << ConvertNumberToStringWithCommas(samplePosition);
+	}};
+
+	auto PrintSamplePositionInTable{[&counter, &ConvertNumberToStringWithCommas](const std::size_t& samplePosition)
+	{ 
+		const std::size_t columns{5};
+		const std::size_t columnWidth{14};
+
+		if(counter % columns == 0)
+		{
+			std::cout << std::endl;
+		}
+
+		std::cout << std::fixed << std::setw(columnWidth) << ConvertNumberToStringWithCommas(samplePosition);
+
+		++counter;
+	}};
+
+	if(transients.size() < minimumTableSize)
+	{
+		std::for_each(transients.begin(), transients.end(), PrintSamplePositionInLine);
+	}
+	else
+	{
+		std::for_each(transients.begin(), transients.end(), PrintSamplePositionInTable);
+	}
 }
